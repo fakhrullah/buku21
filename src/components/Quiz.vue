3b2b5b5ecf8c2d7c2a60d51d7cac4450728b365d
Fakhrullah <fakhrullah@fajarhac.com>
2017-08-29 07:45:36 +0800

    Add quiz welcome page
    User welcomed for every new quiz.
    Because, they need some mind preparation (kokoro no junbi) before start
    answering quiz.
    
    Welcome page, welcome user.

---
+++ src/components/Quiz.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="quiz">
+    <!-- front page - contain welcome word-->
+    <quiz-welcome-page
+      v-if="isCurrentView('quiz-welcome-page')"
+      :quiz-name="quiz.name"
+      :questions-sum="questionsSum"></quiz-welcome-page>
+    <!-- questions - contain a lot of question -->
+      <!-- question 1 -->
+      <!-- question 2 -->
+      <!-- question 3 -->
+      <!-- question 4 -->
+      <!-- question 5 -->
+    <!-- result page - show how much user got correct -->
+    <!-- review page - show list of question with status correct or wrong, 
+    and clickable to question page 
+    and show user answer and correct answer -->
+    <!-- quiz navigation button. start, next, prev, count, review, menu -->
+  </div>
+</template>
+
+<script>
+import QuizWelcomePage from '@/components/QuizWelcomePage'
+
+export default {
+  name: 'quiz',
+  components: {
+    QuizWelcomePage
+  },
+  data () {
+    return {
+      quiz: {
+        name: 'Kuiz Sejarah'
+      },
+      currentView: 'quiz-welcome-page',
+      questions: []
+    }
+  },
+  computed: {
+    questionsSum () {
+      return parseInt(this.questions.length)
+    }
+  },
+  methods: {
+    isCurrentView (view) {
+      if (view === this.currentView) return true
+      else return false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/QuizWelcomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="quiz-welcome-page">
+    <h2>{{ quizName }}</h2>
+    <p>{{ questionsSum }} soalan</p>
+    <p>Selamat menjawab. Semoga berjaya.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'quiz-welcome-page',
+  props: ['quizName', 'questionsSum']
+}
+</script>
+
+<style>
+
+</style>
