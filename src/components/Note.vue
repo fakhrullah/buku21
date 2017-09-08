3e8be35af733c216de0e2d4c872c1a8704fb158f
Fakhrullah <fakhrullah@fajarhac.com>
2017-09-09 07:19:42 +0800

    Add basic template for note

---
+++ src/components/Note.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="note">
+    <h2 class="note-title">Note Title</h2>
+    <div class="note-content">
+      Note content
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'note'
+}
+</script>
+
+<style>
+.note {
+  text-align: left;
+}
+</style>
