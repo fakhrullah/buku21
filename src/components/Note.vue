d836da50edd6a1fb1c3d642c9be3dadd4ccc4dec
Fakhrullah <fakhrullah@fajarhac.com>
2017-09-10 02:30:48 +0800

    Load note page by params
    Pass params.data to load note page. Params.data is note file name without extension (.json).
    
    Use routes with params in Main because route for note now need params.data to look data file to load note data.
    
    Fix note page to fetch data from url based on passed params.data.
    
    Copy note file data in `static/` dir.

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,20 +1,47 @@
 <template>
   <div class="note">
-    <h2 class="note-title">{{ noteTitle }}</h2>
-    <div class="note-content" v-html="noteContent"></div>
+    <h2 class="note-title">{{ note.title }}</h2>
+    <div class="note-content" v-html="note.content"></div>
   </div>
 </template>
 
 <script>
-import noteContent from '../data/note-content.json'
-
 export default {
   name: 'note',
   data () {
     return {
-      noteTitle: noteContent.title,
-      noteContent: noteContent.content
+      note: {
+        title: 'Loading data ...',
+        content: 'Loading content ...'
+      }
     }
+  },
+  methods: {
+    getNoteData () {
+      let noteFile = this.$route.params.data
+      // let self = this
+      fetch('static/' + noteFile + '.json')
+        .then(response => {
+          // this.note.title = response.json().title
+          // console.log(response.json())
+          return response.json()
+        })
+        .then(data => {
+          // console.log(data.title)
+          // console.log(data.content)
+          this.note = data
+        })
+        .catch(err => {
+          console.log(err)
+          this.note = {
+            title: 'Error: No file',
+            content: 'Error: No content'
+          }
+        })
+    }
+  },
+  created () {
+    this.getNoteData()
   }
 }
 </script> 
